0049f666c49a4e5bbd48b0cb6dff08be458285d0
蔡祐臻 <kellytsai@caiyoukuide-MacBook-Pro.local>
2019-10-06 13:31:22 -0700

    Updates

---
+++ precache-manifest.12641831fc17cb244399823bff0ce035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b943243ff072c469930954f2064e0699",
+    "url": "/personal_website/index.html"
+  },
+  {
+    "revision": "e52f07bb87786ffcd82d",
+    "url": "/personal_website/static/css/main.f58cef99.chunk.css"
+  },
+  {
+    "revision": "9158f20455e77de7992b",
+    "url": "/personal_website/static/js/2.440368b1.chunk.js"
+  },
+  {
+    "revision": "e52f07bb87786ffcd82d",
+    "url": "/personal_website/static/js/main.a74663f0.chunk.js"
+  },
+  {
+    "revision": "66b98143216182867817",
+    "url": "/personal_website/static/js/runtime-main.8809df2a.js"
+  },
+  {
+    "revision": "eabb208c3e827ff2eff339d56cfac51c",
+    "url": "/personal_website/static/media/IMG_6234.eabb208c.JPG"
+  }
+]);
